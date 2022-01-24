4db5dc07fff989b16f4084cc5171d8c3ea1db35e
GRAPEL <323917@dadeschools.net>
2022-01-24 08:55:54 -0500

    selection

---
+++ Course_Selector/myScripts.js
@@ -0,0 +1,83 @@
+var nextYear;
+var theGPA;
+var yourGrade;
+
+function cs()
+{
+    nextYear = document.getElementById("csInput").value;
+    console.log(nextYear);
+    if(nextYear == 1)
+    {
+        document.getElementById("cs_answer").innerHTML = "Wise choice.";
+    }
+    else if(nextYear==2)
+    {
+        document.getElementById("cs_answer").innerHTML = "Don't be Alberto.";
+    }
+    else{
+        document.getElementById("cs_answer").innerHTML = "Choose Yes/No";
+    }
+}
+
+function gpa()
+{
+    theGPA = document.getElementById("gpaInput").value;
+    theGPA = parseFloat(theGPA);
+
+    if(theGPA >= 3.5)
+    {
+        document.getElementById("gpa_answer").innerHTML = "You should take many AP courses.";
+    }
+    else if(theGPA > 3.0)
+    {
+        document.getElementById("gpa_answer").innerHTML = "You should take 1 or 2 APs";
+    }
+    else if(theGPA > 2.0)
+    {
+        document.getElementById("gpa_answer").innerHTML = "Get some tutoring";
+    }
+    else{
+        document.getElementById("gpa_answer").style.color = "red";
+        document.getElementById("gpa_answer").innerHTML = "Ouch";
+    }
+}
+
+function yourGrade()
+{
+    yourGrade = document.getElementById("gradeInput").value;
+    console.log(yourGrade);
+    console.log(theGPA);
+    if(theGPA >= 3.5 && yourGrade == 12)
+    {
+        if(nextYear == 1)
+        {
+            document.getElementById("recommendedCS").innerHTML = "Your last year of HS. Consider AP CSA";
+        }
+        else{
+            document.getElementById("recommendedCS").innerHTML = "Why are you wasting your life.";
+        }
+    }
+    else if(theGPA >= 3.5 && (yourGrade == 11 || yourGrade == 10))
+    {
+        if(nextYear == 1)
+        {
+            document.getElementById("recommendedCS").innerHTML = "Consider AP CSP";
+        }
+        else{
+            document.getElementById("recommendedCS").innerHTML = "Why are you wasting your life.";
+        }
+    }
+    else if((theGPA<2.5 && theGPA < 3.5) && (yourGrade == 10 || yourGrade == 11 || yourGrade == 12))
+    {
+        if(nextYear == 1)
+        {
+            document.getElementById("recommendedCS").innerHTML = "Consider Object Oriented Programming";
+        }
+        else{
+            document.getElementById("recommendedCS").innerHTML = "Why are you wasting your life.";
+        }
+    } 
+    else{
+        document.getElementById("recommendedCS").innerHTML = "Maybe you will be good at film studies.";
+    }
+}
